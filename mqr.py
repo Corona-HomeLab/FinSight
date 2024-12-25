2d1c6c7ef7cf393824e3fb64ba2a63518f840679
test1 <carlosjcorona934@gmail.com>
2024-12-25 13:54:11 -0800

    summary: added mqr functionality and will move on to changing data input and also implementing custom prompt functionality

--- mqr.py
+++ mqr.py
@@ -1,4 +1,5 @@
 from components import components
+from prompts import QUERY_PROMPT, output_parser
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.retrievers.multi_query import MultiQueryRetriever
@@ -8,6 +9,7 @@ import logging
 class MQR(components):
     def __init__(self):
         super().__init__()  # Initialize components parent class
+        self.llm_chain = QUERY_PROMPT | self.llm | output_parser
 
     def load_and_split_document(self, url="https://lilianweng.github.io/posts/2023-06-23-agent/"):
         self.loader = WebBaseLoader(url)
@@ -17,8 +19,8 @@ class MQR(components):
 
     def sample_qa(self):
         self.question = "What are the approaches to Task Decomposition?"
-        self.retriever_from_llm = MultiQueryRetriever.from_llm(
-            retriever=self.vector_store.as_retriever(), llm=self.llm
+        self.retriever_from_llm = MultiQueryRetriever(
+            retriever=self.vector_store.as_retriever(), llm_chain=self.llm_chain, parser_key="lines"
         )
         return self.retriever_from_llm
     
@@ -26,6 +28,9 @@ class MQR(components):
         logging.basicConfig()
         logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO)
 
+
+# Other inputs
+question = "What are the approaches to Task Decomposition?"
 
 mqr = MQR()
 
